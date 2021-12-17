3575efa9af3e7e2d29fbc94517bdd85e921c94e9
MM work <djjean90@gmail.com>
2021-12-17 18:35:06 +0200

    final

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //Script.js
+
+const rightBtn = document.querySelector('#right');
+const leftBtn = document.querySelector('#left');
+const img = document.querySelector('.image-container');
+const imgs = img.querySelectorAll('img');
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx > imgs.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = imgs.length - 1;
+  }
+
+  img.style.transform = `translateX(${-idx * 300}px)`;
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++;
+  changeImage();
+  resetInterval();
+});
+
+leftBtn.addEventListener('click', () => {
+  idx--;
+  changeImage();
+  resetInterval();
+});
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
